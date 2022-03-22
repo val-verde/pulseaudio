181356849d7a5bc6cd44ff4c96ac828bcd78121c
Aaditya Chandrasekhar <aadityac@gmail.com>
2022-03-21 23:49:46 -0700

    [REWORK] Use %q instead of %h in inline neon assembly for pa_mix2_ch4_s16ne_neon in src/pulsecore/mix_neon.c.

--- src/pulsecore/mix_neon.c
+++ src/pulsecore/mix_neon.c
@@ -176,8 +176,8 @@ static void pa_mix2_ch4_s16ne_neon(pa_mix_info streams[], int16_t *data, unsigne
     int32x4_t sv0, sv1;
 
     __asm__ __volatile__ (
-        "vld1.s32 %h[sv0], [%[lin0]]         \n\t"
-        "vld1.s32 %h[sv1], [%[lin1]]         \n\t"
+        "vld1.s32 %q[sv0], [%[lin0]]         \n\t"
+        "vld1.s32 %q[sv1], [%[lin1]]         \n\t"
         : [sv0] "=w" (sv0), [sv1] "=w" (sv1)
         : [lin0] "r" (streams[0].linear), [lin1] "r" (streams[1].linear)
         : /* clobber list */
